99c43261c24724f4be6744ae3e29edaede4f90df
yuuki920 <yuuki.k.1672@gmail.com>
2020-11-21 15:54:20 +0900

    検索機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,14 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :login_set, except: [:index]
   before_action :set_book, only: [:edit, :show]
+  before_action :book_protect, only: [:edit, :update, :destroy]
 
  def index
     if current_user
       @books = Book.includes(:user).where(user_id: current_user)
+      @q = Book.ransack(params[:q])   #検索オブジェクト     
+      @books = @q.result(distinct: true)    #検索結果
     else
       redirect_to new_user_session_path
     end
@@ -34,7 +39,13 @@ class BooksController < ApplicationController
   book = Book.find(params[:id])
   book.destroy
   redirect_to root_path, notice: "本のデータを削除しました"
-end
+ end
+
+ def search
+  @q = Book.search(search_params)
+  @books = @q.result(distinct: true)
+ end
+ 
 
 private
  def book_params
@@ -43,6 +54,24 @@ private
 
  def set_book
   @book = Book.find(params[:id])
-end
+ end
+
+ def login_set
+  unless user_signed_in?
+    redirect_to action: :index
+  end
+ end
+
+ def book_protect
+    set_book
+  unless @book.user_id == current_user.id
+    redirect_to root_path
+  end
+ end
+
+ def search_params
+  params.require(:q).permit!
+ end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "books#index" 
   resources :books
+  get 'search', to: 'books#search'
 end
